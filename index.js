a551581f2fdb670319005b339013826a14a9229c
weiweiishere <weiweiishere@gmail.com>
2018-07-03 18:39:59 -0400

    message_type

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ app.post('/webhook', (req, res) => {
 function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
+  	"messaging_type": "RESPONSE",
     "recipient": {
       "id": sender_psid
     },
