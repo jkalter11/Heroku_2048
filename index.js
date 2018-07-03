c11214430f09f957a6ecd7ca335685ee245c418e
weiweiishere <weiweiishere@gmail.com>
2018-07-03 12:21:13 -0400

    playerId

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/webhook', (req, res) => {
 		  //var playerWon = payload['playerWon'];
 		  if (true) { //playerWon
 		    callSendAPI(
-		      senderId, 
+		      playerId, 
 		      'Congratulations on your victory!', 
 		    );
 
