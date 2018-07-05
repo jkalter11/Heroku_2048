f9c51be8beaffd75748e1be48f44355d09f71dbe
weiweiishere <weiweiishere@gmail.com>
2018-07-05 16:36:11 -0400

    button

--- index.js
+++ index.js
@@ -73,32 +73,32 @@ function callSendAPI(sender_psid, response) {
     "recipient": {
       "id": sender_psid
     },
+   //  "message": {
+   //  "text": "Congratulations!"
+  	// }
     "message": {
-    "text": "Congratulations!"
-  	}
-    // "message": {
-    //   "attachment": {
-    //     "type": "template",
-    //     "payload": {
-    //       "template_type": "generic",
-    //       "elements": [
-    //         {
-    //           "title": "It has been a while since your last game. Time to get back",
-    //           "buttons": [
-    //             {
-    //               "type": "game_play",
-    //               "title": "Play Tic-Tac-Toe.",
-    //               "payload": "{}",
-    //               "game_metadata": {
-    //               //"context_id": "<CONTEXT_ID>"
-    //               }
-    //             }
-    //           ]
-    //         }
-    //       ]
-    //     }
-    //   }
-    // }
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [
+            {
+              "title": "It has been a while since your last game. Time to get back",
+              // "buttons": [
+              //   {
+              //     "type": "game_play",
+              //     "title": "Play Tic-Tac-Toe.",
+              //     "payload": "{}",
+              //     "game_metadata": {
+              //     //"context_id": "<CONTEXT_ID>"
+              //     }
+              //   }
+              // ]
+            }
+          ]
+        }
+      }
+    }
   }
   // Send the HTTP request to the Messenger Platform
   request({
